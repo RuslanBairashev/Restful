32ee6b7cbbd7679ad93e314460e165c96b4a00de
RuslanBairashev <ruslan.bairashev@gmail.com>
2022-04-07 13:21:03 +0300

    Initial commit

---
+++ ex00/Education Center/src/main/java/edu/school21/restful/model/Course.java
@@ -0,0 +1,11 @@
+package edu.school21.restful.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Course {
+    private Date startDate;
+    private Date endDate;
+    private String name;
+    private List<User> teachers;
+}
---
+++ ex00/Education Center/src/main/java/edu/school21/restful/model/User.java
@@ -0,0 +1,9 @@
+package edu.school21.restful.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String role; //enum?
+    private String login;
+    private String password;
+}
