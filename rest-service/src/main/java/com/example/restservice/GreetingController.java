e7be83ed73df7e9bd20e5c42b431d1ccf7bf2945
RuslanBairashev <ruslan.bairashev@gmail.com>
2022-04-11 15:46:45 +0300

    by habr

---
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.restservice;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@Api
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    @ApiOperation("test")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
