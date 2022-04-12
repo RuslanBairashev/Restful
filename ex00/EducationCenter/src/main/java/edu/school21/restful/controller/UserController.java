742dcebd4f6aa777478fe8a6ae18e9c6f04dbbe1
RuslanBairashev <ruslan.bairashev@gmail.com>
2022-04-12 08:36:09 +0300

    swagger works

--- ex00/EducationCenter/src/main/java/edu/school21/restful/controller/UserController.java
+++ ex00/EducationCenter/src/main/java/edu/school21/restful/controller/UserController.java
@@ -2,15 +2,18 @@ package edu.school21.restful.controller;
 
 import edu.school21.restful.dao.UserDAO;
 import edu.school21.restful.model.Usr;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/users")
+@Api(description = "test")
 public class UserController {
 
     private final UserDAO userDAO;
@@ -21,12 +24,14 @@ public class UserController {
     }
 
     @GetMapping
+    @ApiOperation("test1")
     public String index(Model model) {
-        model.addAttribute("users", userDAO.index());
+        //model.addAttribute("users", userDAO.index());
         return "users/index";
     }
 
     @GetMapping("/new")
+    @ApiOperation("test2")
     public String newUser(@ModelAttribute("usr") Usr usr) {
         return "users/new";
     }
--- ex00/EducationCenter/src/main/java/edu/school21/restful/dao/UserDAO.java
+++ ex00/EducationCenter/src/main/java/edu/school21/restful/dao/UserDAO.java
@@ -2,23 +2,21 @@ package edu.school21.restful.dao;
 
 import edu.school21.restful.model.Usr;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class UserDAO {
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public UserDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Usr> index() {
-        return jdbcTemplate.query("SELECT *  FROM Usr",
-                new BeanPropertyRowMapper<>(Usr.class));
-    }
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public UserDAO(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public List<Usr> index() {
+//        return jdbcTemplate.query("SELECT *  FROM Usr",
+//                new BeanPropertyRowMapper<>(Usr.class));
+//    }
 }
